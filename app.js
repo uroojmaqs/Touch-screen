1ed94f45e56d12ec98fd8a51cf93e287eaaec117
uroojmaqs <148434922+uroojmaqs@users.noreply.github.com>
2025-04-09 10:17:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// function App() {
+//   const handleTouch = () => {
+//     alert("Touched!");
+//   };
+
+//   return (
+//     <div
+//       onTouchStart={handleTouch}
+//       style={{
+//         height: "100vh",
+//         background: "#ffe",
+//         display: "flex",
+//         justifyContent: "center",
+//         alignItems: "center",
+//         fontSize: "2rem",
+//       }}
+//     >
+//       👆 Touch the screen!
+//     </div>
+//   );
+// }
+
+// export default App;
